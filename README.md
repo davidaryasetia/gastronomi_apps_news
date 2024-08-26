edfbe509f42896211f5f8abd49ef81c28b3f338e
davidaryasetia <david.aryasetia@gmail.com>
2024-08-26 16:59:41 +0700

    first project

---
+++ README.md
@@ -0,0 +1,3 @@
+# project_gastronomy_frontend
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/project_gastronomy_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_gastronomy_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
